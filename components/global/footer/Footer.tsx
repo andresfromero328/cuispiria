29a574b7bea863639e83f519a3766ae99fc2bdd3
Andres Romero <andresfromero328@gmail.com>
2026-02-24 08:16:32 -0800

    added ongoing message

--- components/global/footer/Footer.tsx
+++ components/global/footer/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="">
-      <p>footer</p>
+    <footer className="flex items-center p-10">
+      <small>This is an ongoing project</small>
     </footer>
   );
 };
